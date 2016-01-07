3a0c56af717c08fa25af12071b48d9515b968320
Attila Vaczy <attila.vaczy@yahoo.com>
2016-01-07 17:58:23 +0100

    javascript html gallery the ugly code

--- week-7/wed/majom.js
+++ week-7/wed/majom.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var pics = [
-  'https://41.media.tumblr.com/84729c469ca431c8c80dad1cd4281205/tumblr_o08zo7SJan1tx21ogo1_540.jpg',
-  'http://lorempixel.com/image_output/food-q-c-640-480-8.jpg',
-  'http://lorempixel.com/400/200/sports/1/',
-  'http://lorempixel.com/g/400/200/',
-  'http://lorempixel.com/image_output/food-h-c-640-959-8.jpg',
-  'http://lorempixel.com/image_output/cats-h-c-640-959-4.jpg',
-  'http://lorempixel.com/image_output/nightlife-h-c-640-959-6.jpg',
-  'http://lorempixel.com/image_output/people-q-c-640-460-8.jpg',
-  'http://lorempixel.com/image_output/technics-q-c-640-460-9.jpg',
-  'http://lorempixel.com/image_output/fashion-q-c-640-460-7.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-5.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-4.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-8.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-9.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-4.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-2.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-7.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-10.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-3.jpg',
+  'http://lorempixel.com/image_output/abstract-q-c-640-480-6.jpg',
 ];
 
 var rightbutton = document.querySelector('.rightbutton');
