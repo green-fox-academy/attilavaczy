4b74289a16bbee47a94b5232b2a7347820bdf281
Attila Vaczy <attila.vaczy@yahoo.com>
2016-01-13 17:37:14 +0100

    todoapp callback and post

--- week-8/wed/workshop/todo.js
+++ week-8/wed/workshop/todo.js
@@ -1,27 +1,27 @@
 'use strict';
 
-function createRequest (url, callback) {
-var probaRequest = new XMLHttpRequest();
-probaRequest.open('GET', url);
-//probaRequest.setRequestHeader('X-Mashape-Key', ACCESS_TOKEN);
-probaRequest.send();
-probaRequest.onreadystatechange = function () {
-  console.log('allapot: ', probaRequest.readyState);
-  if (probaRequest.readyState === 4) {
-    callback(probaRequest.response);
-    }
-  };
-}
 
-var url = 'https://mysterious-dusk-8248.herokuapp.com/todos';
-var todoCallback = function (response) {
-  console.log(JSON.parse(response));
-  var todoArray = JSON.parse(response);
-  todoArray.forEach(function(todoItem) {
-    console.log(todoItem.text);
+var url = "https://mysterious-dusk-8248.herokuapp.com/todos";
+var todoContainer = document.querySelector('.todo-container');
+
+var listCallback = function (response) {
+  var todoItems = JSON.parse(response);
+  todoItems.forEach(function(todoItem){
     var newTodoItem = document.createElement('p');
-    newTodoItem.innerText = 
-  });
+    newTodoItem.innerText = todoItem.text;
+    todoContainer.appendChild(newTodoItem);
+  })
 }
 
-createRequest(url, todoCallback);
+var refresh = function () {
+  createRequest('GET', url, {}, listCallback);
+}
+
+refresh();
+
+var newTodo = JSON.stringify({text: 'ajajajajjj majom'});
+var createTodoCallback = function (response) {
+  refresh();
+}
+
+createRequest('POST', url, newTodo, createTodoCallback);
