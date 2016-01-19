4b9db2332f1fbeefd72102cf9b244d2282a9dddd
Attila Vaczy <attila.vaczy@yahoo.com>
2016-01-19 15:51:03 +0100

    backend magic, mysql server connect to todo app

--- week-8/thu/workshop/items.js
+++ week-8/thu/workshop/items.js
@@ -1,38 +1,43 @@
-"use-strict"
-
+"use-strict";
 var TodoItem = function () {
   this.id = nextId();
   this.text = "";
   this.completed = false;
 }
-
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'testgyuri',
+  password: 'test',
+  database: 'todo'
+});
+connection.connect();
+module.exports = {
+  add: addItem,
+//  get: getUser,
+//  getUserById: getUserById
+};
 TodoItem.prototype.update = function(attributes) {
   this.text = attributes.text || "";
   this.completed = !!attributes.completed;
 };
-
 var currId = 0;
 function nextId() {
   return ++currId;
 }
-
 var items = {};
-
 function getItem(id) {
   return items[id];
 }
-
 function addItem(attributes) {
-  var item = new TodoItem();
-  item.update(attributes);
-  items[item.id] = item;
-  return item;
+  connection.query('INSERT INTO todo SET ?', attributes, function(err, result) {
+    if (err) throw err;
+    console.log(result.insertID);
+  })
 }
-
 function removeItem(id) {
   delete items[id];
 }
-
 function allItems() {
   var values = [];
   for (id in items) {
@@ -40,7 +45,6 @@ function allItems() {
   }
   return values;
 }
-
 module.exports = {
   get: getItem,
   add: addItem,
